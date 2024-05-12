0c3dbbe1b32841dc6d85d07b3778df0a8c019b18
David-Baca <david.baca.ingenieria@gmail.com>
2024-05-11 20:31:16 -0500

    V1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var currentURL = window.location.pathname;
+    if (!currentURL.includes('/Login/') && localStorage.getItem('password') !== 'AZUL') {
+        window.location.href = '../Login/index.html';
+        document.getElementById('content').style.display = 'block';
+    }
+    if (!currentURL.includes('/Targeta/') && localStorage.getItem('password') === 'AZUL') {
+        window.location.href = '../Targeta/index.html';
+        document.getElementById('content').style.display = 'block';
+    }
+function login() {
+    var password = document.getElementById('password').value;
+
+    // Verificar si la contraseña ingresada es "AZUL"
+    if (password === 'AZUL') {
+        // Guardar la contraseña en la caché de la página
+        localStorage.setItem('password', password);
+        location.reload();
+    } else {
+        alert('Contraseña incorrecta');
+    }
+}
+
+function clearCache() {
+    localStorage.removeItem('password');
+    location.reload();
+}
